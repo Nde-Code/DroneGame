bb29e76dc56b6dd93d12b77b72876147933304e1
Nde-Code <nathan.debilloez@outlook.com>
2024-08-06 09:15:09 +0200

    Remove unnecessary namespace.

--- main.cs
+++ main.cs
@@ -111,7 +111,7 @@ namespace DroneGame
             battery.Image = Properties.Resources.accumulator;
             battery.SizeMode = PictureBoxSizeMode.AutoSize;
             battery.Location = new Point(posChoiceX, posChoiceY);
-            battery.Tag = "Battery";  
+            battery.Tag = "Battery";
 
             this.Controls.Add(battery);
 
